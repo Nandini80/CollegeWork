8263f8434d3b63c575eb6743e0b036e92069f7f5
Nandini Jindal <121488571+Nandini80@users.noreply.github.com>
2024-07-24 15:42:28 +0530

    Update reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter s : ";
+    cin >> s;
+    for(int i=s.size()-1 ; i>=0 ; i--)
+        cout << s[i];
+}
